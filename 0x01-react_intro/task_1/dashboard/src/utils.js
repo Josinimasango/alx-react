f8d75509408460f86707082ea1e66c13c1ed46b0
Josinimasango <nellyjossydumako@gmail.com>
2024-07-02 11:38:57 +0000

    updating

--- 0x01-react_intro/task_1/dashboard/src/utils.js
+++
@@ -1,8 +0,0 @@
-// utils.js
-export function getFullYear() {
-  return new Date().getFullYear();
-}
-
-export function getFooterCopy(isIndex) {
-  return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
-}
