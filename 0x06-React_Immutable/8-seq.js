4427f686be00f01193bf07d163c8be65fc029399
Josinimasango <nellyjossydumako@gmail.com>
2024-07-18 12:04:13 +0000

    Lazy Seq

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,7 +1,31 @@
-import immutable from 'immutable';
+const { Seq } = require('immutable');
 
-const { Seq } = immutable;
+function printBestStudents(grades) {
+  const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 
-export default function printBestStudents() {
-    
+  const filtered = Seq(grades)
+    .filter(student => student.score >= 70)
+    .map(student => ({
+      ...student,
+      firstName: capitalize(student.firstName),
+      lastName: capitalize(student.lastName)
+    }));
+
+  console.log(filtered.toJS());
 }
+
+// Example usage
+const grades = {
+  1: {
+    score: 99,
+    firstName: 'guillaume',
+    lastName: 'salva',
+  },
+  2: {
+    score: 65,
+    firstName: 'john',
+    lastName: 'doe',
+  }
+};
+
+printBestStudents(grades);
